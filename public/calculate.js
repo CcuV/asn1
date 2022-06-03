34385c450331b44aabedad740a5f5d3933893750
MidasHand <33980788+MidasHand@users.noreply.github.com>
2022-06-03 04:41:01 -0700

    Update calculate.js

--- public/calculate.js
+++ public/calculate.js
@@ -1,82 +1,46 @@
-let grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03,
-  49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
-
-function validateGrade (g) {
-  g = Number(g);
-  if (g > 100 || g < 0) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-let letterGrades = [];
-let letterRows = document.querySelectorAll('.left .row');
-let letterResults = document.querySelectorAll('.top .num');
-
-function initLetterGrades () {
-  letterGrades = [];
-  letterRows.forEach(function (row) {
-    letterGrades.push({
-      name: row.querySelector('.type').textContent,
-      value: Number(row.querySelector('input').value)
-    });
-  });
-  console.log(letterGrades);
-  drawResult();
-}
-
-initLetterGrades();
-
-document.getElementById('new').addEventListener('keydown', function (e) {
-  if (e.key === 'Enter') {
-    if (validateGrade(e.target.value)) {
-      grades.push(Number(e.target.value));
-      e.target.value = '';
-      drawResult();
-    } else {
-      window.alert('Please input a score > 0 and < 100');
+var grades = [
+  65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.0, 81.43, 86.22, 88.33,
+  9.03, 49.93, 52.34, 53.11, 50.1, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54,
+  90.0, 71.11, 80.01,
+];
+renderHistogram();
+document.querySelector("input").addEventListener("keypress", (value) => {
+  console.log(value);
+  if (value.code === "Enter") {
+    if (!isNaN(Number(value.target.value))) {
+      grades.push(value.target.value);
+      renderHistogram();
     }
   }
 });
 
-document.querySelectorAll('.left .row input').forEach(function (input, index) {
-  input.addEventListener('change', function (e) {
-    let num = Number(e.target.value);
-
-    if (index > 0 && num >= letterGrades[index - 1].value) {
-      window.alert('Input value should not bigger or equal than value of ' + letterGrades[index - 1].name);
-      e.target.value = letterGrades[index].value.toFixed(2);
-    } else if (index !== letterGrades.length - 1 && num <= letterGrades[index + 1].value) {
-      window.alert('Input value should not smaller or equal than value of ' + letterGrades[index + 1].name);
-      e.target.value = letterGrades[index].value.toFixed(2);
-    } else if (validateGrade(e.target.value)) {
-      initLetterGrades();
-    } else {
-      window.alert('Please input a score > 0 and < 100');
-      e.target.value = letterGrades[index].value.toFixed(2);
+function renderHistogram() {
+  Array.from(document.querySelectorAll(".histogram-item")).forEach(
+    (item) => (item.innerHTML = "")
+  );
+  grades.forEach((item) => {
+    if (item < 50) {
+      document.querySelector(".histogram-item-0").innerHTML += "O";
+    } else if (item >= 50 && item < 55) {
+      document.querySelector(".histogram-item-50").innerHTML += "O";
+    } else if (item >= 55 && item < 60) {
+      document.querySelector(".histogram-item-55").innerHTML += "O";
+    } else if (item >= 60 && item < 65) {
+      document.querySelector(".histogram-item-60").innerHTML += "O";
+    } else if (item >= 65 && item < 70) {
+      document.querySelector(".histogram-item-65").innerHTML += "O";
+    } else if (item >= 70 && item < 75) {
+      document.querySelector(".histogram-item-70").innerHTML += "O";
+    } else if (item >= 75 && item < 80) {
+      document.querySelector(".histogram-item-75").innerHTML += "O";
+    } else if (item >= 80 && item < 85) {
+      document.querySelector(".histogram-item-80").innerHTML += "O";
+    } else if (item >= 85 && item < 90) {
+      document.querySelector(".histogram-item-85").innerHTML += "O";
+    } else if (item >= 90 && item < 95) {
+      document.querySelector(".histogram-item-90").innerHTML += "O";
+    } else if (item >= 95 && item < 100) {
+      document.querySelector(".histogram-item-95").innerHTML += "O";
     }
-  });
-});
-
-function drawResult () {
-  letterGrades.forEach(function (g) {
-    g.count = 0;
-  });
-  grades.forEach(function (g) {
-    for (let grade of letterGrades) {
-      if (grade.name === 'Max') {
-        // ignore Max
-        continue;
-      }
-      if (g >= grade.value) {
-        grade.count++;
-        return;
-      }
-    }
-  });
-
-  letterResults.forEach(function (result, index) {
-    result.style.width = letterGrades[index + 1].count * 50 + 'px';
   });
 }
